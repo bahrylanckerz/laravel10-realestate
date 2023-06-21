a23a31153a8de6e4216f117131870ea94da9196a
Bahry Lanckerz <bahrylanckerz@gmail.com>
2023-06-21 22:50:23 +0700

    edit admin form profile

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -7,7 +7,43 @@
         </ol>
     </nav>
     <div class="row">
-        <div class="col-12 grid-margin stretch-card">
+        <div class="d-none d-md-block col-md-4 left-wrapper">
+            <div class="card rounded">
+                <div class="card-body text-center">
+                    <img class="wd-100 rounded-circle mb-2" src="{{ (!empty($user->photo)) ? asset('uploads/img/'.$user->photo) : asset('uploads/img/no_image.jpg') }}" alt="profile">
+                    <br>
+                    <span class="h4">{{ $user->name }}</span>
+                    <div class="mt-3">
+                        <label class="tx-11 fw-bolder mb-0 text-uppercase">Joined:</label>
+                        <p class="text-muted">{{ date('j F Y', strtotime($user->created_at)) }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="tx-11 fw-bolder mb-0 text-uppercase">Role:</label>
+                        <p class="text-muted">{{ ucfirst($user->role) }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="tx-11 fw-bolder mb-0 text-uppercase">Status:</label>
+                        <p class="text-muted">{{ ucfirst($user->status) }}</p>
+                    </div>
+                    <div class="mt-3">
+                        <label class="tx-11 fw-bolder mb-0 text-uppercase">Website:</label>
+                        <p class="text-muted">www.nobleui.com</p>
+                    </div>
+                    <div class="mt-3 d-flex justify-content-center social-links">
+                        <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                        </a>
+                        <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+                        </a>
+                        <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
                     <h6 class="card-title">Profile Information</h6>
@@ -33,14 +69,6 @@
                         <div class="mb-3">
                             <label for="address" class="form-label">Address</label>
                             <textarea id="address" name="address" class="form-control" autocomplete="off" rows="3">{{ old('address') ?: $user->address }}</textarea>
-                        </div>
-                        <div class="mb-3">
-                            <label for="role" class="form-label">Role</label>
-                            <input type="text" id="role" name="role" class="form-control" autocomplete="off" value="{{ ucfirst($user->role) }}" disabled>
-                        </div>
-                        <div class="mb-3">
-                            <label for="status" class="form-label">Status</label>
-                            <input type="text" id="status" name="status" class="form-control" autocomplete="off" value="{{ ucfirst($user->status) }}" disabled>
                         </div>
                         <button type="submit" class="btn btn-primary btn-icon-text">
                             <i class="btn-icon-prepend" data-feather="send"></i>
